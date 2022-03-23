15a95c30fd47502def979fc260f7c44f01b658fd
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-23 12:43:13 -0300

    refactor change title index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Meta
-        title="Home teste"
+        title="Ecommerce Demo Next.js"
         seoTitle="Ecommerce Demo React Next.js"
         seoDescription="Ecommece demo React Next.js Desenvolvimento de sites com tecnologias Modernas"
       />
