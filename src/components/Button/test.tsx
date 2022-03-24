22593bfb1cd352d9041e09d214b22d763df304d5
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-24 16:01:56 -0300

    test: should render the medium button size

---
+++ src/components/Button/test.tsx
@@ -0,0 +1,15 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Button from '.'
+
+describe('<Button />', () => {
+  it('should render the medium size by default', () => {
+    renderWithTheme(<Button>Comprar agora</Button>)
+    expect(screen.getByRole('button', { name: /Comprar agora/i })).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem'
+    })
+  })
+})
