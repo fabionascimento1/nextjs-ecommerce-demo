a46210fb8dacf6c59f74accc598677daf04ead4b
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-26 09:37:55 -0300

    test: create menu

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -29,4 +29,29 @@ describe('<Menu />', () => {
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
+  it('should render register box when logged out', () => {
+    renderWithTheme(<Menu />)
+
+    /* expect(screen.getByText(/faça seu login/i)).toBeInTheDocument() */
+    expect(
+      screen.getByText(/Crie sua conta com segurança/i)
+    ).toBeInTheDocument()
+
+    expect(screen.queryByText(/Acesse sua conta/i)).not.toBeInTheDocument()
+    expect(
+      screen.queryByText(/Acesse sua lista de desejos/i)
+    ).not.toBeInTheDocument()
+  })
+
+  it('should render register box when logged in', () => {
+    renderWithTheme(<Menu username="Fabio" />)
+
+    expect(screen.getByText(/Acesse sua conta/i)).toBeInTheDocument()
+    expect(screen.getByText(/Acesse sua lista de desejos/i)).toBeInTheDocument()
+
+    expect(screen.queryByText(/faça seu login/i)).not.toBeInTheDocument()
+    expect(
+      screen.queryByText(/Crie sua conta com segurança/i)
+    ).not.toBeInTheDocument()
+  })
 })
