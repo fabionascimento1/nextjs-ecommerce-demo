190e78bbb69cdf05389f305f5ab7704fbae182df
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-26 14:45:30 -0300

    feat: add Home template

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from 'components/Container'
+import Menu from 'components/Menu'
+import React from 'react'
+
+const Home = () => (
+  <Container>
+    <Menu />
+  </Container>
+)
+
+export default Home
