55eeafd4faacc71eb58ff0e0a0b2ab80a290180a
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-26 15:22:49 -0300

    fix: name correct styled

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 import { ThemeProvider } from 'styled-components'
-import GlobalStyles from 'styles/global.styles'
+import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
 export const parameters = {
