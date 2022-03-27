4e2ddf53afcd4fae21306111626b540dbf2f1b28
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-27 12:26:42 -0300

    test: add GameCard

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -1,4 +1,5 @@
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
+import theme from 'styles/theme'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameCard from '.'
@@ -26,5 +27,59 @@ describe('<GameCard />', () => {
       'src',
       props.img
     )
+  })
+
+  it('should render price in label', () => {
+    renderWithTheme(<GameCard {...props} />)
+
+    const price = screen.getByText('235')
+
+    expect(price).not.toHaveStyle({ textDecoration: 'line-through' })
+    expect(price).toHaveStyle({ backgroundColor: theme.colors.primary })
+  })
+
+  it('should render a line-through in price when promotional', () => {
+    renderWithTheme(<GameCard {...props} promotionalPrice={15} />)
+
+    expect(screen.getByText('235')).toHaveStyle({
+      textDecoration: 'line-through'
+    })
+
+    expect(screen.getByText('15')).not.toHaveStyle({
+      textDecoration: 'line-through'
+    })
+  })
+
+  it('should render a gamr for favorite', () => {
+    renderWithTheme(<GameCard {...props} favorite />)
+
+    expect(
+      screen.getByLabelText(/Remover da lista de desejos/i)
+    ).toBeInTheDocument()
+  })
+
+  it('should call onFav method when favorite is clicked', () => {
+    const onFav = jest.fn()
+    renderWithTheme(<GameCard {...props} favorite onFav={onFav} />)
+
+    fireEvent.click(screen.getAllByRole('button')[0])
+
+    expect(onFav).toBeCalled()
+  })
+
+  it('should render Ribbon', () => {
+    renderWithTheme(
+      <GameCard
+        {...props}
+        ribbon="My Ribbon"
+        ribbonColor="secondary"
+        ribbonSize="small"
+      />
+    )
+    const ribbon = screen.getByText(/my ribbon/i)
+
+    expect(ribbon).toHaveStyle({ backgroundColor: '#6dfdfc' })
+    expect(ribbon).toHaveStyle({ height: '2.6rem', fontSize: '1.2rem' })
+    expect(ribbon).toBeInTheDocument()
   })
 })
