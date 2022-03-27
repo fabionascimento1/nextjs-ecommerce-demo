3380d79c3030310edce0a1041782c26d313dfedb
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-27 11:24:13 -0300

    test: add GameCard

---
+++ src/components/GameCard/test.tsx
@@ -0,0 +1,30 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameCard from '.'
+
+const props = {
+  title: 'Population Zero',
+  developer: 'Rockstar Games',
+  img: 'https://source.unsplash.com/user/willianjusten/300x140',
+  price: 235
+}
+
+describe('<GameCard />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<GameCard {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: props.developer })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+  })
+})
