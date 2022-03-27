c8f4e6d8f1987fd919b2d9f64858251df5c2801f
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-27 08:08:34 -0300

    test: insert Ribon in Banner Component

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -25,4 +25,24 @@ describe('<Banner />', () => {
 
     expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
   })
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <Banner
+        {...props}
+        ribbon="My Ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+
+    const ribbon = screen.getByText(/My Ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({ backgroundColor: '#6dfdfc' })
+    expect(ribbon).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
+  })
 })
