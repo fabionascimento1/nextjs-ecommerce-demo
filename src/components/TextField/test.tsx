0069365f3974cdc5a4967f0f5fd02a0f9cbb8385
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-28 15:31:29 -0300

    test: add TextField

---
+++ src/components/TextField/test.tsx
@@ -0,0 +1,118 @@
+import { screen, waitFor } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import userEvent from '@testing-library/user-event'
+import { Email } from '@styled-icons/material-outlined'
+
+import TextField from '.'
+
+describe('<TextField />', () => {
+  it('Renders with Label', () => {
+    renderWithTheme(<TextField label="Label" name="Label" />)
+
+    expect(screen.getByLabelText('Label')).toBeInTheDocument()
+  })
+
+  it('Renders without Label', () => {
+    renderWithTheme(<TextField />)
+
+    expect(screen.queryByLabelText('Label')).not.toBeInTheDocument()
+  })
+
+  it('Renders with placeholder', () => {
+    renderWithTheme(<TextField placeholder="hey you" />)
+
+    expect(screen.getByPlaceholderText('hey you')).toBeInTheDocument()
+  })
+
+  it('Renders with Icon', () => {
+    renderWithTheme(<TextField icon={<Email data-testid="icon" />} />)
+
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
+  })
+
+  it('Renders with Icon on the right side', () => {
+    renderWithTheme(
+      <TextField icon={<Email data-testid="icon" />} iconPosition="right" />
+    )
+
+    expect(screen.getByTestId('icon').parentElement).toHaveStyle({ order: 1 })
+  })
+
+  it('Changes its value when typing', async () => {
+    const onInputChange = jest.fn()
+    renderWithTheme(
+      <TextField
+        onInputChange={onInputChange}
+        label="TextField"
+        name="TextField"
+      />
+    )
+
+    const input = screen.getByRole('textbox')
+    const text = 'This is my new text'
+    userEvent.type(input, text)
+
+    await waitFor(() => {
+      expect(input).toHaveValue(text)
+      expect(onInputChange).toHaveBeenCalledTimes(text.length)
+    })
+    expect(onInputChange).toHaveBeenCalledWith(text)
+  })
+
+  it('Does not changes its value when disabled', async () => {
+    const onInputChange = jest.fn()
+    renderWithTheme(
+      <TextField
+        onInputChange={onInputChange}
+        label="TextField"
+        name="TextField"
+        disabled
+      />
+    )
+
+    const input = screen.getByRole('textbox')
+    expect(input).toBeDisabled()
+
+    const text = 'This is my new text'
+    userEvent.type(input, text)
+
+    await waitFor(() => {
+      expect(input).not.toHaveValue(text)
+    })
+    expect(onInputChange).not.toHaveBeenCalled()
+  })
+
+  it('Renders with error', () => {
+    const { container } = renderWithTheme(
+      <TextField
+        icon={<Email data-testid="icon" />}
+        label="TextField"
+        error="Error message"
+      />
+    )
+
+    expect(screen.getByText('Error message')).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('Is accessible by tab', () => {
+    renderWithTheme(<TextField label="TextField" name="TextField" />)
+
+    const input = screen.getByLabelText('TextField')
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+    expect(input).toHaveFocus()
+  })
+
+  it('Is not accessible by tab when disabled', () => {
+    renderWithTheme(<TextField label="TextField" name="TextField" disabled />)
+
+    const input = screen.getByLabelText('TextField')
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+    expect(input).not.toHaveFocus()
+  })
+})
