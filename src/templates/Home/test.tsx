bded92fad8abe03aed524f681bf3688529326d9d
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-28 12:31:14 -0300

    test: ensure render section elements at HomeTemplate

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -11,8 +11,8 @@ import Home from '.'
 const props = {
   banners: bannerMock,
   mostPopularHighLight: highLightMock,
-  newGames: gamesMock,
-  mostPopularGames: gamesMock
+  newGames: [gamesMock[0]],
+  mostPopularGames: [gamesMock[0]]
 }
 
 describe('<Home />', () => {
@@ -28,5 +28,16 @@ describe('<Home />', () => {
     expect(
       screen.getByRole('heading', { name: /Mais Populares/i })
     ).toBeInTheDocument()
+  })
+  it('should render section elements', () => {
+    renderWithTheme(<Home {...props} />)
+    // banner
+    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
+    // card game ( 5 sections com 4 cards cada = 5x4 = 20)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(2)
+    // card game ( 5 sections com 1 card cada = 5x1 = 5)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(2)
+    // highlight
+    expect(screen.getAllByText(/read dead is back!/i)).toHaveLength(2)
   })
 })
