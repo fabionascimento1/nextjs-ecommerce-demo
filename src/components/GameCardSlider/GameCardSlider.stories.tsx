b0df15962f9bdda4348c4c9b8ac14baec8587ed6
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-28 06:08:11 -0300

    feat: add GameCardSlider component

---
+++ src/components/GameCardSlider/GameCardSlider.stories.tsx
@@ -0,0 +1,23 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import { GameCardProps } from 'components/GameCard/types'
+import GameCardSlider from '.'
+import items from './mock'
+
+export default {
+  title: 'GameCardSlider',
+  component: GameCardSlider,
+  args: { items },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta<GameCardProps>
+
+export const Default: Story<GameCardProps[]> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <GameCardSlider items={args} {...args} />
+  </div>
+)
