042d0d2023f2d798e18d49e2da2727db6a03d135
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-28 07:25:25 -0300

    feat: add new session in Home template

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -3,22 +3,26 @@ import Menu from 'components/Menu'
 import Footer from 'components/Footer'
 import React from 'react'
 import Highlight from 'components/Highlight'
-
-const props = {
-  title: 'Heading 1',
-  subtitle: 'Heading 2',
-  backgroundImage: '/img/red-dead-img.jpg',
-  floatImage: '/img/red-dead-float.png',
-  buttonLabel: 'Buy now',
-  buttonLink: '/rdr2'
-}
+import Heading from 'components/Heading'
 
 const Home = () => (
-  <Container>
-    <Menu />
-    <Highlight {...props} alignment="left" />
-    <Footer />
-  </Container>
+  <>
+    <Container>
+      <Menu />
+    </Container>
+
+    <Container>
+      <Heading>Novidades</Heading>
+    </Container>
+
+    <Container>
+      <Heading>Mais Populares</Heading>
+    </Container>
+
+    <Container>
+      <Footer />
+    </Container>
+  </>
 )
 
 export default Home
