f7dda6e8dc20e4f3ed744d78121a271492b8d413
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-29 06:01:50 -0300

    feat: change links colors

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -44,12 +44,13 @@ export const FormLink = styled.div`
     a {
       color: ${theme.colors.secondary};
       text-decoration: none;
-      border-bottom: 0.1rem solid ${theme.colors.secondary};
+      border-bottom: 0.1rem solid ${theme.colors.bgColor};
+      color: ${theme.colors.bgColor};
       transition: color, border, ${theme.transition.fast};
 
       &:hover {
-        color: ${darken(0.1, theme.colors.secondary)};
-        border-bottom: 0.1rem solid ${darken(0.1, theme.colors.secondary)};
+        color: ${darken(0.1, theme.colors.bgColor)};
+        border-bottom: 0.1rem solid ${darken(0.1, theme.colors.bgColor)};
       }
     }
   `}
