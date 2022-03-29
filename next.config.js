9335d6cd36166e22ec7f4b5ab715b0207751681e
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-29 11:21:21 -0300

    feat: add new url domains images

--- next.config.js
+++ next.config.js
@@ -8,6 +8,6 @@ module.exports = withPWA({
     disable: !isProd
   },
   images: {
-    domains: ['localhost', 'res.cloudinary.com']
+    domains: ['localhost', 'res.cloudinary.com', 'images.gog-statics.com']
   }
 })
