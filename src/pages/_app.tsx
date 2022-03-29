faa63d8e91ba6f0e81bed73b18b8935f2a0362e0
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-29 11:20:24 -0300

    feat: add next-seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 
+import { DefaultSeo } from 'next-seo'
+import SEO from '../../next-seo.config'
+
 import { ThemeProvider } from 'styled-components'
 import theme from 'styles/theme'
 import GlobalStyle from 'styles/global'
@@ -8,6 +11,7 @@ import GlobalStyle from 'styles/global'
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <DefaultSeo {...SEO} />
       <GlobalStyle />
       <Component {...pageProps} />
     </ThemeProvider>
