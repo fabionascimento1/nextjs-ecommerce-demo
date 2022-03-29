9f00ef0675bd98e270f4c6c360b3a7188f5249d5
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-29 11:20:33 -0300

    feat: add next-seo

---
+++ next-seo.config.js
@@ -0,0 +1,22 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://wongames.willianjusten.com.br',
+    site_name: 'Won Games',
+    title: 'Won Games',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/wongames/image/upload/v1619472565/won_games_og_fc420ed09b.png',
+        width: 2560,
+        height: 1440,
+        alt: 'Won Games'
+      }
+    ]
+  },
+  twitter: {
+    handle: '@Willian_justen',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
