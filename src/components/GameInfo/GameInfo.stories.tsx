f004cd0cc7f2d6ae3ca6bdc8f2009f53ff3bdbf5
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-29 09:21:22 -0300

    feat: add GameInfo components

---
+++ src/components/GameInfo/GameInfo.stories.tsx
@@ -0,0 +1,15 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import GameInfo, { GameInfoProps } from '.'
+import mockGame from './mock'
+
+export default {
+  title: 'Game/GameInfo',
+  component: GameInfo,
+  args: mockGame
+} as Meta<GameInfoProps>
+
+export const Default: Story<GameInfoProps> = (args) => (
+  <div style={{ maxWidth: '144rem', margin: 'auto', padding: '1.5rem' }}>
+    <GameInfo {...args} />
+  </div>
+)
---
+++ src/components/GameInfo/index.tsx
@@ -0,0 +1,39 @@
+import {
+  AddShoppingCart,
+  FavoriteBorder
+} from '@styled-icons/material-outlined'
+
+import Button from 'components/Button'
+import Heading from 'components/Heading'
+import Ribbon from 'components/Ribbon'
+import * as S from './styles'
+
+export type GameInfoProps = {
+  id: string
+  title: string
+  description: string
+  price: number
+}
+
+const GameInfo = ({ id, title, description, price }: GameInfoProps) => (
+  <S.Wrapper data-cy="game-info">
+    <Heading reverseColor lineBottom>
+      {title}
+    </Heading>
+
+    <Ribbon color="secondary">{price}</Ribbon>
+
+    <S.Description>{description}</S.Description>
+
+    <S.ButtonsWrapper>
+      <Button icon={<AddShoppingCart />} size="large">
+        Add to cart
+      </Button>
+      <Button icon={<FavoriteBorder />} size="large" minimal>
+        Wishlist
+      </Button>
+    </S.ButtonsWrapper>
+  </S.Wrapper>
+)
+
+export default GameInfo
