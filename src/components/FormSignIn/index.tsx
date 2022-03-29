cdc6682ed41e447453be130d1bc4ca571bcd3bfe
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-28 21:38:42 -0300

    feat: add FormSign

---
+++ src/components/FormSignIn/index.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+import { Email, Lock, ErrorOutline } from '@styled-icons/material-outlined'
+
+import { FormLink, FormWrapper, FormLoading, FormError } from 'components/Form'
+import Button from 'components/Button'
+import TextField from 'components/TextField'
+
+import * as S from './styles'
+
+const FormSignIn = () => {
+  return (
+    <FormWrapper>
+      <form>
+        <TextField
+          name="email"
+          placeholder="Email"
+          type="email"
+          error=""
+          icon={<Email />}
+        />
+        <TextField
+          name="password"
+          placeholder="Password"
+          type="password"
+          error=""
+          icon={<Lock />}
+        />
+        <Link href="/forgot-password" passHref>
+          <S.ForgotPassword>Forgot your password?</S.ForgotPassword>
+        </Link>
+
+        <Button type="submit" size="large" fullWidth>
+          enviar
+        </Button>
+
+        <FormLink>
+          Don’t have an account?{' '}
+          <Link href="/sign-up">
+            <a>Sign up</a>
+          </Link>
+        </FormLink>
+      </form>
+    </FormWrapper>
+  )
+}
+
+export default FormSignIn
