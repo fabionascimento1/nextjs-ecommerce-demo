4b87f11a256f6d8247e22a85c4b9a1fa5720c78e
Fabio Nascimento <agsuperpoder@gmail.com>
2022-04-02 14:19:25 -0300

    test: add ProfileMenu Component

---
+++ src/components/ProfileMenu/test.tsx
@@ -0,0 +1,38 @@
+import { screen } from '@testing-library/react'
+import theme from 'styles/theme'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import ProfileMenu from '.'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+
+useRouter.mockImplementation(() => ({
+  query: {}
+}))
+
+describe('<ProfileMenu />', () => {
+  it('should render the menu', () => {
+    const { container } = renderWithTheme(<ProfileMenu />)
+
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /my orders/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /sign out/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the menu with an active link defined', () => {
+    renderWithTheme(<ProfileMenu activeLink="/profile/orders" />)
+
+    expect(screen.getByRole('link', { name: /my orders/i })).toHaveStyle({
+      background: theme.colors.primary,
+      color: theme.colors.white
+    })
+  })
+})
