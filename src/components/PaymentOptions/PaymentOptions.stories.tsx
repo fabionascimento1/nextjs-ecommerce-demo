b235ffd52cd821b77bbd7baa3998bba52e427215
Fabio Nascimento <agsuperpoder@gmail.com>
2022-04-02 11:55:51 -0300

    feat: add PaymentOptions Component

---
+++ src/components/PaymentOptions/PaymentOptions.stories.tsx
@@ -0,0 +1,31 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import PaymentOptions, { PaymentOptionsProps } from '.'
+
+import cardsMock from './mock'
+
+export default {
+  title: 'Components/PaymentOptions',
+  component: PaymentOptions,
+  args: {
+    cards: cardsMock
+  },
+  argTypes: {
+    cards: {
+      type: ''
+    },
+    handlePayment: {
+      action: 'clicked'
+    }
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<PaymentOptionsProps> = (args) => (
+  <div style={{ padding: 16, maxWidth: 400 }}>
+    <PaymentOptions {...args} />
+  </div>
+)
---
+++ src/components/PaymentOptions/mock.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    flag: 'visa',
+    img: '/img/cards/visa.png',
+    number: '*** *** **** 4325'
+  },
+  {
+    flag: 'mastercard',
+    img: '/img/cards/mastercard.png',
+    number: '*** *** **** 4326'
+  }
+]
