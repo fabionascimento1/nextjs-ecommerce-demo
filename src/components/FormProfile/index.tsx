9c3544903626599e1a8a0fbd06e77b7b544437b1
Fabio Nascimento <agsuperpoder@gmail.com>
2022-04-02 14:49:11 -0300

    feat: add FormProfile  Component

---
+++ src/components/FormProfile/index.tsx
@@ -0,0 +1,48 @@
+import Button from 'components/Button'
+import Heading from 'components/Heading'
+import TextField from 'components/TextField'
+import Link from 'next/link'
+
+import * as S from './styles'
+
+export type FormProfileProps = {
+  username?: string
+  email?: string
+}
+
+const FormProfile = ({ email, username }: FormProfileProps) => (
+  <>
+    <Heading lineBottom size="small">
+      My profile
+    </Heading>
+
+    <S.Form>
+      <TextField
+        name="username"
+        placeholder="Username"
+        label="Username"
+        initialValue={username}
+      />
+
+      <TextField
+        name="email"
+        type="email"
+        placeholder="E-mail"
+        initialValue={email}
+        label="E-mail"
+        disabled
+      />
+
+      <S.ButtonContainer>
+        <Link href={`/forgot-password?email=${email}`} passHref>
+          <Button minimal size="medium" as="a">
+            Reset Password
+          </Button>
+        </Link>
+        <Button size="medium">Save</Button>
+      </S.ButtonContainer>
+    </S.Form>
+  </>
+)
+
+export default FormProfile
