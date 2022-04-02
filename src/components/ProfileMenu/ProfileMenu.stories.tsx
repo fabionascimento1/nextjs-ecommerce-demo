e3b9b687bd8827df831eda6795c022adfc074eea
Fabio Nascimento <agsuperpoder@gmail.com>
2022-04-02 14:19:15 -0300

    feat: add ProfileMenu Component

---
+++ src/components/ProfileMenu/ProfileMenu.stories.tsx
@@ -0,0 +1,16 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import ProfileMenu, { ProfileMenuProps } from '.'
+
+export default {
+  title: 'Profile/ProfileMenu',
+  component: ProfileMenu,
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta<ProfileMenuProps>
+
+export const Default: Story<ProfileMenuProps> = (args) => (
+  <ProfileMenu {...args} />
+)
---
+++ src/components/ProfileMenu/index.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+
+import {
+  AccountCircle,
+  ExitToApp,
+  FormatListBulleted
+} from '@styled-icons/material-outlined'
+
+import * as S from './styles'
+
+export type ProfileMenuProps = {
+  activeLink?: '/profile/me' | '/profile/cards' | '/profile/orders' | string
+}
+
+const ProfileMenu = ({ activeLink }: ProfileMenuProps) => {
+  return (
+    <S.Nav>
+      <Link href="/profile/me" passHref>
+        <S.Link isActive={activeLink === '/profile/me'} title="My profile">
+          <AccountCircle size={24} />
+          <span>My profile</span>
+        </S.Link>
+      </Link>
+
+      <Link href="/profile/orders" passHref>
+        <S.Link isActive={activeLink === '/profile/orders'} title="My orders">
+          <FormatListBulleted size={24} />
+          <span>My orders</span>
+        </S.Link>
+      </Link>
+
+      <S.Link
+        role="button"
+        onClick={async () => {
+          const data = ''
+        }}
+      >
+        <ExitToApp size={24} title="Sign out" />
+        <span>Sign out</span>
+      </S.Link>
+    </S.Nav>
+  )
+}
+
+export default ProfileMenu
