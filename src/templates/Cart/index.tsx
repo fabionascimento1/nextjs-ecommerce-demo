e43a13bc36c611d1a16c2806fd7c22aac8097553
Fabio Nascimento <agsuperpoder@gmail.com>
2022-04-02 13:33:57 -0300

    feat: add Cart Template

---
+++ src/templates/Cart/index.tsx
@@ -0,0 +1,58 @@
+import CartList, { CartListProps } from 'components/CartList'
+import { Container } from 'components/Container'
+import { Divider } from 'components/Divider'
+import Empty from 'components/Empty'
+import { GameCardProps } from 'components/GameCard/types'
+import Heading from 'components/Heading'
+import { HighlightProps } from 'components/Highlight/types'
+import PaymentOptions, { PaymentOptionsProps } from 'components/PaymentOptions'
+import Showcase from 'components/Showcase'
+import React from 'react'
+import Base from 'templates/Base'
+
+import * as S from './styles'
+
+export type CartProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+} & CartListProps &
+  Pick<PaymentOptionsProps, 'cards'>
+
+export default function Cart({
+  recommendedGames,
+  recommendedHighlight,
+  items,
+  total,
+  cards
+}: CartProps) {
+  const handlePayment = () => ({})
+
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary" reverseColor>
+          Meu Carinho
+        </Heading>
+        {items.length ? (
+          <S.Content>
+            <CartList items={items} total={total} />
+
+            <PaymentOptions cards={cards} handlePayment={handlePayment} />
+          </S.Content>
+        ) : (
+          <Empty
+            title="Your cart is empty"
+            description="Go back to the store and explore great games and offers"
+            hasLink
+          />
+        )}
+        <Divider />
+      </Container>
+      <Showcase
+        title="You may like these games"
+        games={recommendedGames}
+        highlight={recommendedHighlight}
+      />
+    </Base>
+  )
+}
