0d5843accd88f4c9d2261588baaab9cb07041272
Fabio Nascimento <agsuperpoder@gmail.com>
2022-04-02 09:42:32 -0300

    feat: add GameInfo Component

---
+++ src/components/GameItem/index.tsx
@@ -0,0 +1,82 @@
+import Image from 'next/image'
+import { Download } from '@styled-icons/boxicons-solid/Download'
+
+import * as S from './styles'
+import { useState } from 'react'
+
+export type PaymentInfoProps = {
+  number: string
+  flag: string | null
+  img: string | null
+  purchaseDate: string
+}
+
+export type GameItemProps = {
+  id: string
+  img: string
+  title: string
+  price: string
+  downloadLink?: string
+  paymentInfo?: PaymentInfoProps
+}
+
+const GameItem = ({
+  id,
+  img,
+  title,
+  price,
+  downloadLink,
+  paymentInfo
+}: GameItemProps) => {
+  const [isInCart, removeFromCart] = useState([])
+
+  return (
+    <S.Wrapper data-cy="game-item">
+      <S.GameContent>
+        <S.ImageBox>
+          <Image src={img} alt={title} width={150} height={70} />
+        </S.ImageBox>
+
+        <S.Content>
+          <S.Title>
+            {title}
+            {!!downloadLink && (
+              <S.DownloadLink
+                href={downloadLink}
+                target="_blank"
+                aria-label={`Get ${title} here`}
+              >
+                <Download size={22} />
+              </S.DownloadLink>
+            )}
+          </S.Title>
+          <S.Group>
+            <S.Price>{price}</S.Price>
+            {isInCart && (
+              <S.Remove onClick={() => removeFromCart}>Remove</S.Remove>
+            )}
+          </S.Group>
+        </S.Content>
+      </S.GameContent>
+
+      {!!paymentInfo && (
+        <S.PaymentContent>
+          <p>{paymentInfo.purchaseDate}</p>
+          <S.CardInfo>
+            <span>{paymentInfo.number}</span>
+            {!!paymentInfo.img && !!paymentInfo.flag && (
+              <Image
+                src={paymentInfo.img}
+                alt={paymentInfo.flag}
+                width={38}
+                height={24}
+              />
+            )}
+          </S.CardInfo>
+        </S.PaymentContent>
+      )}
+    </S.Wrapper>
+  )
+}
+
+export default GameItem
