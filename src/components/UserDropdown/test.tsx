1d450e8b4eb1e3aefb1b2b98e75c9d417d16f4a2
Fabio Nascimento <agsuperpoder@gmail.com>
2022-04-03 14:24:36 -0300

    test: add UserDropdown Component

---
+++ src/components/UserDropdown/test.tsx
@@ -0,0 +1,33 @@
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import UserDropdown from '.'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+
+useRouter.mockImplementation(() => ({
+  query: {}
+}))
+
+describe('<UserDropdown />', () => {
+  it('should render the username', () => {
+    renderWithTheme(<UserDropdown username="Willian" />)
+
+    expect(screen.getByText(/willian/i)).toBeInTheDocument()
+  })
+
+  it('should render the menu', () => {
+    renderWithTheme(<UserDropdown username="Willian" />)
+
+    // open menu
+    userEvent.click(screen.getByText(/willian/i))
+
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /wishlist/i })).toBeInTheDocument()
+  })
+})
