304ff445d34d881dcd3790a7a349e76923da147e
Fabio Nascimento <agsuperpoder@gmail.com>
2022-04-03 10:28:08 -0300

    feat: add OrdersList component

---
+++ src/components/OrdersList/index.tsx
@@ -0,0 +1,42 @@
+import Empty from 'components/Empty'
+import GameItem, { GameItemProps, PaymentInfoProps } from 'components/GameItem'
+import Heading from 'components/Heading'
+import * as S from './styles'
+
+type OrderProps = {
+  id: string
+  paymentInfo: PaymentInfoProps
+  games: GameItemProps[]
+}
+
+export type OrdersListProps = {
+  items?: OrderProps[]
+}
+
+const OrdersList = ({ items = [] }: OrdersListProps) => (
+  <S.Wrapper>
+    <Heading lineBottom lineColor="primary" size="small">
+      My orders
+    </Heading>
+
+    {items.length ? (
+      items.map((order) => {
+        return order.games.map((game) => (
+          <GameItem
+            key={`${order.id}-${game.id}`}
+            {...game}
+            paymentInfo={order.paymentInfo}
+          />
+        ))
+      })
+    ) : (
+      <Empty
+        title="You have no orders yet"
+        description="Go back to the store and explore great games and offers"
+        hasLink
+      />
+    )}
+  </S.Wrapper>
+)
+
+export default OrdersList
---
+++ src/components/OrdersList/mock.ts
@@ -0,0 +1,40 @@
+export default [
+  {
+    id: '1',
+    paymentInfo: {
+      flag: 'visa',
+      img: '/img/cards/visa.png',
+      number: '**** **** **** 4242',
+      purchaseDate: 'Purchase made on Apr 14, 2021'
+    },
+    games: [
+      {
+        id: '1',
+        title: 'game',
+        downloadLink:
+          'https://wongames.com/game/download/yuYT56Tgh431LkjhNBgdf',
+        img: 'https://source.unsplash.com/user/willianjusten/151x70',
+        price: '$10.00'
+      }
+    ]
+  },
+  {
+    id: '2',
+    paymentInfo: {
+      flag: 'visa',
+      img: '/img/cards/mastercard.png',
+      number: '**** **** **** 4444',
+      purchaseDate: 'Purchase made on Apr 14, 2021'
+    },
+    games: [
+      {
+        id: '2',
+        title: 'game',
+        downloadLink:
+          'https://wongames.com/game/download/yuYT56Tgh431LkjhNBgdf',
+        img: 'https://source.unsplash.com/user/willianjusten/151x70',
+        price: '$10.00'
+      }
+    ]
+  }
+]
